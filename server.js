dd738ef2ced37c18a2ccbc33641e8969095bbd1a
Bundy Kim <bundy@mahalo.com>
2011-08-27 00:08:57 -0700

    getting socket io importing correctly on the client

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express'),
     nko = require('nko')('ifZu/MT8VJF/wtlB');
-
 var app = express.createServer();
+var io = require('socket.io').listen(app);
 
 app.use('/media', express.static(__dirname + '/media'));
 app.use('/', express.static(__dirname + '/'));
 
 app.listen(7777);
 console.log('8bitbeats! Listening on port ' + app.address().port);
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function(data) {
+        console.log(data);
+    });
+});
