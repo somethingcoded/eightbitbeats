840d5f0dea88424beff747b228c7ff151e2b4a5b
Bundy Kim <bundy@mahalo.com>
2011-08-26 22:46:43 -0700

    adding express and serving of our static files

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var http = require('http'),
+var express = require('express'),
     nko = require('nko')('ifZu/MT8VJF/wtlB');
 
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('8bitbeats!');
-});
-app.listen(parseInt(process.env.PORT) || 7777);
-console.log('Listening on ' + app.address().port);
+var app = express.createServer();
+
+app.use('/media', express.static(__dirname + '/media'));
+app.use('/', express.static(__dirname + '/'));
+
+app.listen(7777);
+console.log('8bitbeats! Listening on port ' + app.address().port);
