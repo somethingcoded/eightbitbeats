a6deaa70b6e0470711a124932e8c13a17986b11e
Bundy Kim <bundy@mahalo.com>
2011-08-28 05:51:14 -0700

    adding sound assets

---
+++ sfx/square/square.js
@@ -0,0 +1,32 @@
+{
+	'instrument': {
+		'name': 'square',
+		'sounds': [
+			{ 'name': 'c5', 'filename': 'square2_short_C5.mp3'},
+			{ 'name': 'b4', 'filename': 'square2_short_B4.mp3'},
+			{ 'name': 'a#4', 'filename': 'square2_short_A#4.mp3'},
+			{ 'name': 'a4', 'filename': 'square2_short_A4.mp3'},
+			{ 'name': 'g#4', 'filename': 'square2_short_G#4.mp3'},
+			{ 'name': 'g4', 'filename': 'square2_short_G4.mp3'},
+			{ 'name': 'f#4', 'filename': 'square2_short_F#4.mp3'},
+			{ 'name': 'f4', 'filename': 'square2_short_F4.mp3'},
+			{ 'name': 'e4', 'filename': 'square2_short_E4.mp3'},
+			{ 'name': 'd#4', 'filename': 'square2_short_D#4.mp3'},
+			{ 'name': 'd4', 'filename': 'square2_short_D4.mp3'},
+			{ 'name': 'c#4', 'filename': 'square2_short_C#4.mp3'},
+			{ 'name': 'c4', 'filename': 'square2_short_C4.mp3'},
+			{ 'name': 'b3', 'filename': 'square2_short_B3.mp3'},
+			{ 'name': 'a#3', 'filename': 'square2_short_A#3.mp3'},
+			{ 'name': 'a3', 'filename': 'sine_short_A3.mp3'},
+			{ 'name': 'g#3', 'filename': 'square2_short_G#3.mp3'},
+			{ 'name': 'g3', 'filename': 'square2_short_G3.mp3'},
+			{ 'name': 'f#3', 'filename': 'square2_short_F#3.mp3'},
+			{ 'name': 'f3', 'filename': 'square2_short_F3.mp3'},
+			{ 'name': 'e3', 'filename': 'square2_short_E3.mp3'},
+			{ 'name': 'd#3', 'filename': 'square2_short_D#3.mp3'},
+			{ 'name': 'd3', 'filename': 'square2_short_D3.mp3'},
+			{ 'name': 'c#3', 'filename': 'square2_short_C#3.mp3'},
+			{ 'name': 'c3', 'filename': 'square2_short_C3.mp3'}
+		]
+	}	
+}
